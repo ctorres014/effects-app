2514b8627f4ec0b61b06b0e7c907850076119496
ctorres014 <ctorres014@gmail.com>
2019-01-24 01:00:00 -0300

    first commit app sin effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+// modulos personalizados
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    UsuariosModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
